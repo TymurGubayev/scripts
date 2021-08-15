a8450c57d7cb38398abbc1112fc5b027e3102749
Timur Kelman <tymur.gubayev@gmail.com>
2021-08-15 16:19:08 +0200

    fix autodocs

--- gui/petitions.lua
+++ gui/petitions.lua
@@ -5,7 +5,7 @@ gui/petitions
 =============
 Show fort's petitions, pending and fulfilled.
 
-For best experience add following to your `dfhack*.init`::
+For best experience add following to your ``dfhack*.init``::
 
     keybinding add Alt-P@dwarfmode/Default gui/petitions
 
